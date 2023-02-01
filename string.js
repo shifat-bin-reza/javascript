48d7de2fde569ddd105e9c96805605c2a242f9aa
shifat-bin-reza <m.shifatbinreza@gmail.com>
2023-02-01 21:19:34 +0600

    string problem

---
+++ string.js
@@ -0,0 +1,19 @@
+const name = "Wakanda Forever";
+
+console.log(name.length);
+
+console.log(name[4]);
+
+console.log(name.toLowerCase());
+
+console.log(name.toUpperCase());
+
+console.log(name.includes("Wakanda"));
+
+console.log(name.includes("Wakandaa"));
+
+console.log(name.indexOf("Forever"));
+
+console.log(name.startsWith("Wakanda"));
+
+console.log(name.startsWith("Forever"));
